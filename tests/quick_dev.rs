996f62551d6c12e7cc44050b32d72e6be6c4063b
Ixtlan <ixtlan@gmail.com>
2025-12-25 12:04:03 +0100

    Add postgres and minijinja

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,10 +1,10 @@
 #![allow(unused)]
 
-#[tokio::test]
-async fn quick_dev() -> Result<()> {
-    // let hc = httpc_test::new_client("http://localhost:5050")?;
+// #[tokio::test]
+// async fn quick_dev() -> Result<()> {
+//     // let hc = httpc_test::new_client("http://localhost:5050")?;
 
-    // hc.do_get("/hello").await?.print().await?;
+//     // hc.do_get("/hello").await?.print().await?;
 
-    Ok(())
-}
+//     Ok(())
+// }
